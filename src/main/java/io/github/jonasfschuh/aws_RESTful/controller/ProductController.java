cc3e7c425b2043942ca7d95efbda9ac481a93703
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-15 13:02:35 -0300

    created ProductController

--- src/main/java/io/github/jonasfschuh/aws_RESTful/controller/ProductController.java
+++ src/main/java/io/github/jonasfschuh/aws_RESTful/controller/ProductController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -37,6 +34,12 @@ public class ProductController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> savePRoduct(@RequestBody Product product) {
+        productRepository.save(product);
+        return new ResponseEntity<Product>(product, HttpStatus.CREATED);
     }
 
 
