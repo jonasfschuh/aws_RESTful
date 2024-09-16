e3a620ccc6e84521f3d506eecfd74f79d8a72860
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-16 14:35:58 -0300

    release 1.0.11

---
+++ src/main/java/io/github/jonasfschuh/aws_RESTful/config/SnsConfig.java
@@ -0,0 +1,33 @@
+package io.github.jonasfschuh.aws_RESTful.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.Topic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SnsConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Value("${aws.sns.topic.product.events.arn}")
+    private String productEventsTopic;
+
+    @Bean
+    public AmazonSNS snsClient() {
+        return AmazonSNSClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
+
+    @Bean
+    public Topic snsProductEventsTopic() {
+        return new Topic().withTopicArn(productEventsTopic);
+    }
+
+}
