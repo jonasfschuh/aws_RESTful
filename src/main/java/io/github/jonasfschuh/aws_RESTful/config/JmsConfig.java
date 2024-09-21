5f12a679284a3738bbf068d7d6bae82949755005
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-21 19:58:37 -0300

    updated JmsConfig

--- src/main/java/io/github/jonasfschuh/aws_RESTful/config/JmsConfig.java
+++ src/main/java/io/github/jonasfschuh/aws_RESTful/config/JmsConfig.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.support.destination.DynamicDestinationResolver;
@@ -15,6 +16,7 @@ import javax.jms.Session;
 
 @Configuration
 @EnableJms
+@Profile("!local")
 public class JmsConfig {
 
     @Value("${aws.region}")
