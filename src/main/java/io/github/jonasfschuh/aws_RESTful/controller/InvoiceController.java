dec1a5111cf489a219844b27611fb4e35ca105de
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-21 16:12:42 -0300

    update InvoiceController

--- src/main/java/io/github/jonasfschuh/aws_RESTful/controller/InvoiceController.java
+++ src/main/java/io/github/jonasfschuh/aws_RESTful/controller/InvoiceController.java
@@ -1,10 +1,19 @@
 package io.github.jonasfschuh.aws_RESTful.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/invoices")
 public class InvoiceController {
+
+    @Value("${aws.s3.bucket.invoice.name}")
+    private String bucketName;
+
+    private AmazonS3 amazonS3;
+
+
 
 }
