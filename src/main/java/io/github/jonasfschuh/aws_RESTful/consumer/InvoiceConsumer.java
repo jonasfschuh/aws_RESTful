b0720c9e6164d432e98fdfb940fd9928b4076266
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-21 17:38:04 -0300

    updated InvoiceConsumer

--- src/main/java/io/github/jonasfschuh/aws_RESTful/consumer/InvoiceConsumer.java
+++ src/main/java/io/github/jonasfschuh/aws_RESTful/consumer/InvoiceConsumer.java
@@ -1,7 +1,9 @@
 package io.github.jonasfschuh.aws_RESTful.consumer;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.event.S3EventNotification;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.jonasfschuh.aws_RESTful.model.SnsMessage;
 import io.github.jonasfschuh.aws_RESTful.repository.InvoiceRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +35,29 @@ public class InvoiceConsumer {
     public void receiveS3Event(TextMessage textMessage) throws JMSException, IOException {
 
         log.info("Invoice event received - Message: {}", textMessage);
+        SnsMessage snsMessage = objectMapper.readValue(textMessage.getText(),
+                SnsMessage.class);
+
+        S3EventNotification s3EventNotification = objectMapper
+                .readValue(snsMessage.getMessage(), S3EventNotification.class);
+
+        processInvoiceNotification(s3EventNotification);
+
+    }
+
+    private void processInvoiceNotification(S3EventNotification s3EventNotification) throws IOException {
+
+        for (S3EventNotification.S3EventNotificationRecord
+             s3EventNotificationRecord : s3EventNotification.getRecords()) {
+            S3EventNotification.S3Entity s3Entity = s3EventNotificationRecord.getS3();
+
+            String bucketName = s3Entity.getBucket().getName();
+            String objectKey = s3Entity.getObject().getKey();
+
+
+        }
+
+
     }
 
 
