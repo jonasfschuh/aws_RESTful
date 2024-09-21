efd2a0b0d4ac40bb1f0ffebfcbd5a1e7422be78c
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-21 15:45:16 -0300

    created S3Config

---
+++ src/main/java/io/github/jonasfschuh/aws_RESTful/config/S3Config.java
@@ -0,0 +1,23 @@
+package io.github.jonasfschuh.aws_RESTful.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
+}
