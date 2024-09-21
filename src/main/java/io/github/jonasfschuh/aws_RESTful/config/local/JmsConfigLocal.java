09e33cb2f413c621c8651141d508b06d43add522
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-21 20:55:21 -0300

    updated JmsConfigLocal

--- src/main/java/io/github/jonasfschuh/aws_RESTful/config/local/JmsConfigLocal.java
+++ src/main/java/io/github/jonasfschuh/aws_RESTful/config/local/JmsConfigLocal.java
@@ -6,6 +6,7 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQSClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -19,6 +20,8 @@ import javax.jms.Session;
 @EnableJms
 @Profile("local")
 public class JmsConfigLocal {
+    @Value("${aws.region}")
+    private String awsRegion;
 
     private SQSConnectionFactory sqsConnectionFactory;
 
